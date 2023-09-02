0a8ab60a1dd51faa1a42050bc3fef71fcba39f55
yonghun16 <yonghun16@naver.com>
2023-09-02 21:36:26 +0900

    수정함:        config.lua

--- config.lua
+++ config.lua
@@ -152,9 +152,9 @@ lvim.builtin.which_key.mappings["e"] = {}
 
 -- ESC 키를 편하게 <Ctrl+f>로 변경
 lvim.keys.normal_mode["<C-f>"] = "<ESC>"
-lvim.keys.term_mode["<C-f>"] = "<C-\\><C-n><RIGHT>"
-lvim.keys.term_mode["<ESC>"] = "<C-\\><C-n><RIGHT>"
-lvim.keys.insert_mode["<C-f>"] = "<RIGHT><ESC>"
+lvim.keys.term_mode["<C-f>"] = "<C-\\><C-n>"
+lvim.keys.term_mode["<ESC>"] = "<C-\\><C-n>"
+lvim.keys.insert_mode["<C-f>"] = "<ESC>"
 lvim.keys.visual_mode["<C-f>"] = "<ESC>"
 lvim.keys.command_mode["<C-f>"] = "<ESC>"
 
