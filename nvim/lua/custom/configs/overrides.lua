bb716e32f58487b0298bafddbcd6eade26364b9b
yonghun16 <yonghun16@naver.com>
2024-02-28 23:56:29 +0900

    overrides update

---
+++ nvim/lua/custom/configs/overrides.lua
@@ -0,0 +1,184 @@
+---------------------------------------------------------------------------
+-- VimScript plugins config
+---------------------------------------------------------------------------
+-- visual-multi
+vim.cmd([[ let g:VM_maps = {} ]])
+vim.cmd([[ let g:VM_maps["Find Under"]   = '<C-/>' ]])
+vim.cmd([[ let g:VM_maps["Find Subword Under"]   = '<C-/>' ]])
+vim.cmd([[ let g:VM_maps["Add Cursor At Pos"]   = '<C-RIGHT>' ]])
+
+-- emmet-vim
+vim.g.user_emmet_leader_key = '<C-,>'
+
+-- tagbar
+vim.cmd([[ let g:tagbar_width = '30' ]])
+local M = {}
+
+---------------------------------------------------------------------------
+-- Lua plugins config
+---------------------------------------------------------------------------
+M.noice = {
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true,         -- use a classic bottom cmdline for search
+    command_palette = true,       -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,       -- add a border to hover docs and signature help
+  },
+}
+
+M.jabs = {
+  position = { 'center', 'center' }, -- position = {'<position_x>', '<position_y>'} | <position_x> left, center, right,
+
+  relative = 'editor',               -- win, editor, cursor. Default win
+  clip_popup_size = false,           -- clips the popup size to the win (or editor) size. Default true
+
+  width = 80,                        -- default 50
+  height = 20,                       -- default 10
+  border = 'single',                 -- none, single, double, rounded, solid, shadow, (or an array or chars). Default shadow
+
+  offset = {                         -- window position offset
+    top = 2,                         -- default 0
+    bottom = 2,                      -- default 0
+    left = 2,                        -- default 0
+    right = 2,                       -- default 0
+  },
+
+  sort_mru = true,                -- Sort buffers by most recently used (true or false). Default false
+  split_filename = true,          -- Split filename into separate components for name and path. Default false
+  split_filename_path_width = 20, -- If split_filename is true, how wide the column for the path is supposed to be, Default 0 (don't show path)
+
+  -- Options for preview window
+  preview_position = 'top', -- top, bottom, left, right. Default top
+  preview = {
+    width = 85,             -- default 70
+    height = 70,            -- default 30
+    border = 'single',      -- none, single, double, rounded, solid, shadow, (or an array or chars). Default double
+  },
+
+  -- Default highlights (must be a valid :highlight)
+  highlight = {
+    current = "Title",       -- default StatusLine
+    hidden = "StatusLineNC", -- default ModeMsg
+    split = "WarningMsg",    -- default StatusLine
+    alternate = "StatusLine" -- default WarningMsg
+  },
+
+  -- Default symbols
+  symbols = {
+    current = "C",         -- default 
+    split = "S",           -- default 
+    alternate = "A",       -- default 
+    hidden = "H",          -- default ﬘
+    locked = "L",          -- default 
+    ro = "R",              -- default 
+    edited = "E",          -- default 
+    terminal = "T",        -- default 
+    default_file = "D",    -- Filetype icon if not present in nvim-web-devicons. Default 
+    terminal_symbol = ">_" -- Filetype icon for a terminal split. Default 
+  },
+
+  -- Keymaps
+  keymap = {
+    close = "dd",  -- Close buffer. Default D
+    jump = "<CR>", -- Jump to buffer. Default <cr>
+    h_split = "h", -- Horizontally split buffer. Default s
+    v_split = "v", -- Vertically split buffer. Default v
+    preview = "p", -- Open buffer preview. Default P
+  },
+
+  -- Whether to use nvim-web-devicons next to filenames
+  use_devicons = false -- true or false. Default true
+}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "json",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+
+    -- shell stuff
+    "shfmt",
+  },
+}
+
+-- git support in nvimtree
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- OR use all default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- remove a default
+  vim.keymap.del('n', 's', { buffer = bufnr })
+
+  -- add your mappings
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+  ---
+end
+
+M.nvimtree = {
+  on_attach = my_on_attach,
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+
+return M
--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,40 +1,6 @@
 local overrides = require("custom.configs.overrides")
 
 ---------------------------------------------------------------------------
--- plugin keymapping options
----------------------------------------------------------------------------
--- nvim-tree
-local function my_on_attach(bufnr)
-  local api = require('nvim-tree.api')
-
-  local function opts(desc)
-    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
-  -- OR use all default mappings
-  api.config.mappings.default_on_attach(bufnr)
-
-  -- remove a default
-  vim.keymap.del('n', 's', { buffer = bufnr })
-
-  -- add your mappings
-  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-  ---
-end
-
--- visual-multi
-vim.cmd([[ let g:VM_maps = {} ]])
-vim.cmd([[ let g:VM_maps["Find Under"]   = '<C-/>' ]])
-vim.cmd([[ let g:VM_maps["Find Subword Under"]   = '<C-/>' ]])
-vim.cmd([[ let g:VM_maps["Add Cursor At Pos"]   = '<C-RIGHT>' ]])
-
--- emmet-vim
-vim.g.user_emmet_leader_key = '<C-,>'
-
--- tagbar
-vim.cmd([[ let g:tagbar_width = '30' ]])
-
----------------------------------------------------------------------------
 -- Plugins
 ---------------------------------------------------------------------------
 ---@type NvPluginSpec[]
@@ -42,106 +8,19 @@ local plugins = {
   -- JABS.nvim
   {
     "matbme/JABS.nvim",
-    config = function()
-      require 'jabs'.setup {
-        -- Options for the main window
-        position = { 'center', 'top' }, -- position = {'<position_x>', '<position_y>'} | <position_x> left, center, right,
-
-        relative = 'editor', -- win, editor, cursor. Default win
-        clip_popup_size = false, -- clips the popup size to the win (or editor) size. Default true
-
-        width = 80,          -- default 50
-        height = 20,         -- default 10
-        border = 'single',   -- none, single, double, rounded, solid, shadow, (or an array or chars). Default shadow
-
-        offset = {           -- window position offset
-          top = 2,           -- default 0
-          bottom = 2,        -- default 0
-          left = 2,          -- default 0
-          right = 2,         -- default 0
-        },
-
-        sort_mru = true,            -- Sort buffers by most recently used (true or false). Default false
-        split_filename = true,      -- Split filename into separate components for name and path. Default false
-        split_filename_path_width = 20, -- If split_filename is true, how wide the column for the path is supposed to be, Default 0 (don't show path)
-
-        -- Options for preview window
-        preview_position = 'left', -- top, bottom, left, right. Default top
-        preview = {
-          width = 40,          -- default 70
-          height = 60,         -- default 30
-          border = 'single',   -- none, single, double, rounded, solid, shadow, (or an array or chars). Default double
-        },
-
-        -- Default highlights (must be a valid :highlight)
-        highlight = {
-          current = "Title",     -- default StatusLine
-          hidden = "StatusLineNC", -- default ModeMsg
-          split = "WarningMsg",  -- default StatusLine
-          alternate = "StatusLine" -- default WarningMsg
-        },
-
-        -- Default symbols
-        symbols = {
-          current = "C",       -- default 
-          split = "S",         -- default 
-          alternate = "A",     -- default 
-          hidden = "H",        -- default ﬘
-          locked = "L",        -- default 
-          ro = "R",            -- default 
-          edited = "E",        -- default 
-          terminal = "T",      -- default 
-          default_file = "D",  -- Filetype icon if not present in nvim-web-devicons. Default 
-          terminal_symbol = ">_" -- Filetype icon for a terminal split. Default 
-        },
-
-        -- Keymaps
-        keymap = {
-          close = "dd", -- Close buffer. Default D
-          jump = "<CR>", -- Jump to buffer. Default <cr>
-          h_split = "h",  -- Horizontally split buffer. Default s
-          v_split = "v",  -- Vertically split buffer. Default v
-          preview = "p",  -- Open buffer preview. Default P
-        },
-
-        -- Whether to use nvim-web-devicons next to filenames
-        use_devicons = false -- true or false. Default true
-      }
-    end,
+    opts = overrides.jabs,
     cmd = "JABSOpen",
   },
 
   -- noice.nvim
   {
     "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- add any options here
-    },
     dependencies = {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
     },
-    config = function()
-      require("noice").setup({
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true,         -- use a classic bottom cmdline for search
-          command_palette = true,       -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false,       -- add a border to hover docs and signature help
-        },
-      })
-    end
+    opts = overrides.noice,
+    event = "VeryLazy",
   },
 
   -- vim-lastplace
@@ -227,15 +106,6 @@ local plugins = {
     end
   },
 
-  -- nvim-navbuddy
-  {
-    "SmiteshP/nvim-navbuddy",
-    requires = {
-      "neovim/nvim-lspconfig",
-      "SmiteshP/nvim-navic",
-    },
-  },
-
   -- toggleterm.nvim
   {
     "akinsho/toggleterm.nvim",
@@ -282,7 +152,7 @@ local plugins = {
     event = "VimEnter",
   },
 
-  -- nvim-lspconfig
+  -- nvim-navbuddy
   {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -295,10 +165,6 @@ local plugins = {
         opts = { lsp = { auto_attach = true } }
       }
     },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
   },
 
   -- mason.nvim
@@ -307,11 +173,11 @@ local plugins = {
     opts = overrides.mason
   },
 
-  -- -- nvim-treesitter
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = overrides.treesitter,
-  -- },
+  -- nvim-treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
 
   -- nvim-treesitter-textobjects
   {
@@ -321,11 +187,7 @@ local plugins = {
   -- nvim-tree
   {
     "nvim-tree/nvim-tree.lua",
-    config = function()
-      require("nvim-tree").setup({
-        on_attach = my_on_attach,
-      })
-    end
+    opts = overrides.nvimtree,
   },
 
   -- conform.nvim
