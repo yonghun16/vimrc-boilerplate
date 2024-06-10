fc6021b00152dca014652030febba0b990924003
Frontend16 <yonghun16@naver.com>
2024-06-10 14:37:25 +0900

    Update mappings.lua

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,12 +1,6 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
-
--- map("n", ";", ":", { desc = "CMD enter command mode" })
--- map("i", "jk", "<ESC>")
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 -- ESC
 map({"n", "v", "c"}, "<C-f>", "<ESC>")
