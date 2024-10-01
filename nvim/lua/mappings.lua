634e3b76b1b2021f369cc66caed20ed679c1a9ec
yonghun16 <yonghun16@naver.com>
2024-10-01 11:45:48 +0900

    update

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,7 +8,7 @@ local map = vim.keymap.set
 map("n", "<leader>q", "<cmd>q<CR>", {desc = "Quit"})
 map("n", "<leader>Q", "<cmd>q!<CR>", {desc = "Quit with save"})
 
--- cursor movement
+-- Cursor movement
 map({"n", "v"}, "<C-k>", "3k")
 map({"n", "v"}, "<C-j>", "3j")
 map({"n", "v"}, "<C-l>", "$")
@@ -16,11 +16,25 @@ map({"n", "v"}, "<C-h>", "^")
 map({"n", "v"}, "<C-s-k>", "10k")
 map({"n", "v"}, "<C-s-j>", "10j")
 
--- page movement
+-- Page movement
 map({"n", "v"}, "<C-n>", "3<C-e>")
 map({"n", "v"}, "<C-p>", "3<C-y>")
 map({"n", "v"}, "<C-f>", "<C-d>")
 map({"n", "v"}, "<C-d>", "<C-u>")
+
+-- Window movement
+map("n", "<D-k>", "<C-w>k")
+map("n", "<D-j>", "<C-w>j")
+map("n", "<D-l>", "<C-w>l")
+map("n", "<D-h>", "<C-w>h")
+
+-- Resize window
+map("n", "<D-p>", "<C-w>2+")
+map("n", "<D-n>", "<C-w>2-")
+map("n", "<D-.>", "<C-w>2>")
+map("n", "<D-,>", "<C-w>2<")
+map("n", "<D-m>", "<C-w>=" )
+map("n", "<D-S-c>", "<C-w>c" )
 
 -- Sidebar
 map("n", "<leader>h", "<cmd>NvimTreeToggle<CR>", {desc = "Nvimtree"})
@@ -44,20 +58,13 @@ map("i", "<C-u>", "<ESC>d^xi")
 map("i", "<C-k>", "<ESC><Right>C")
 map("i", "<C-CR>", "<ESC>o")
 
--- TERMINAL Mode keymap
+-- Terminal Mode keymap
 map("t", "<leader>,", "<cmd>:ToggleTerm<CR>")
 map("t", "<ESC>", "<C-\\><C-n>")
 map("t", "<C-w>k", "<C-\\><C-n><C-w>k")
 map("t", "<C-w>l", "<C-\\><C-n><C-w>l")
 map("t", "<C-w>j", "<C-\\><C-n><C-w>j")
 map("t", "<C-w>h", "<C-\\><C-n><C-w>h")
-
--- Resize window
-map("n", "<A-k>", "<C-w>2+")
-map("n", "<A-j>", "<C-w>2-")
-map("n", "<A-l>", "<C-w>2>")
-map("n", "<A-h>", "<C-w>2<")
-map("n", "<A-m>", "<C-w>=" )
 
 -- Tab
 map("n", "te", "<cmd>tabedit<CR>")
