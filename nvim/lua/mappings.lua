98ac75e89770919cca46519a15ca1debf3dbf763
yonghun16 <yonghun16@naver.com>
2025-02-10 20:23:14 +0900

    Update mappings.lua

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,13 +5,13 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 -- Modify existing keymap
-vim.api.nvim_del_keymap("n", "<leader>e")  -- for nvimtree
-vim.api.nvim_del_keymap("n", "<leader>ds")  -- for diagnostics list
-vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { noremap = true, expr = true })  -- for Codeium(Disable Autocomplete)
-vim.api.nvim_set_keymap("n", "<C-i>", "<C-i>", { noremap = true, silent = true })  -- Separate <Tab>and <C-i>
+vim.api.nvim_del_keymap("n", "<leader>e") -- for nvimtree
+vim.api.nvim_del_keymap("n", "<leader>ds") -- for diagnostics list
+vim.api.nvim_set_keymap("i", "<Tab>", 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { noremap = true, expr = true }) -- for Codeium(Disable Autocomplete)
+vim.api.nvim_set_keymap("n", "<C-i>", "<C-i>", { noremap = true, silent = true }) -- Separate <Tab>and <C-i>
 
 -- ESC
-map({"n", "v", "i"}, "<C-c>", "<ESC>")
+map({ "n", "i", "v", "c" }, "<C-c>", "<ESC>")
 map("t", "<ESC>", "<C-\\><C-n>")
 
 -- Quit
@@ -20,18 +20,18 @@ map("n", "<leader>q", "<cmd>q<CR>", { desc = "quit" })
 map("n", "<leader>Q", "<cmd>q!<CR>", { desc = "quit with save" })
 
 -- Cursor movement
-map({"n", "v"}, "<C-k>", "3k")
-map({"n", "v"}, "<C-j>", "3j")
-map({"n", "v"}, "<C-l>", "$")
-map({"n", "v"}, "<C-h>", "^")
+map({ "n", "v" }, "<C-k>", "3k")
+map({ "n", "v" }, "<C-j>", "3j")
+map({ "n", "v" }, "<C-l>", "$")
+map({ "n", "v" }, "<C-h>", "^")
 
 -- Screen movement
-map({"n", "v"}, "<C-n>", "3<C-e>")
-map({"n", "v"}, "<C-p>", "3<C-y>")
-map({"n", "v"}, "<C-f>", "<C-d>")
-map({"n", "v"}, "<C-d>", "<C-u>")
-map({"n", "v"}, "<C-S-l>", "3zl")
-map({"n", "v"}, "<C-S-h>", "3zh")
+map({ "n", "v" }, "<C-n>", "3<C-e>")
+map({ "n", "v" }, "<C-p>", "3<C-y>")
+map({ "n", "v" }, "<C-f>", "<C-d>")
+map({ "n", "v" }, "<C-d>", "<C-u>")
+map({ "n", "v" }, "<C-S-l>", "5zl")
+map({ "n", "v" }, "<C-S-h>", "5zh")
 
 -- Split window movement
 map("n", "<D-k>", "<C-w>k")
@@ -44,8 +44,8 @@ map("n", "<D-p>", "<C-w>2+")
 map("n", "<D-n>", "<C-w>2-")
 map("n", "<D-.>", "<C-w>2>")
 map("n", "<D-,>", "<C-w>2<")
-map("n", "<D-m>", "<C-w>=" )
-map("n", "<D-S-c>", "<C-w>c" )
+map("n", "<D-m>", "<C-w>=")
+map("n", "<D-S-c>", "<C-w>c")
 
 -- Tab page
 map("n", "te", "<cmd>tabedit<CR>")
@@ -61,15 +61,15 @@ map("n", "<leader>L", "<cmd>TagbarToggle<CR>", { desc = "tagbar" })
 
 -- Floating window functions
 map("n", "<leader>k", "<cmd>Navbuddy<CR>", { desc = "navbuddy" })
-map("n", "<leader>f", "<cmd>lua require('fzf-lua').files()<CR>", { desc="FZF file explorer" })
+map("n", "<leader>f", "<cmd>lua require('fzf-lua').files()<CR>", { desc = "FZF file explorer" })
 map("n", "<leader><leader>", "<cmd>ToggleTerm direction=float<CR>", { desc = "terminal(floating)" })
 map("n", "<leader><tab>", "<cmd>JABSOpen<CR>", { desc = "show buffers" })
 
 -- Toggle functions
-map("n", "tw", "<cmd>lua ToggleWrapCodes()<CR>")  -- Wrap codes
-map("n", "ta", "<cmd>lua ToggleAIAutoComplete()<CR>")  -- AI Auto complete
+map("n", "tw", "<cmd>lua ToggleWrapCodes()<CR>") -- Wrap codes
+map("n", "ta", "<cmd>lua ToggleAIAutoComplete()<CR>") -- AI Auto complete
 map("n", "tf", "<cmd>lua ToggleFoldColumn()<CR>") -- Foldcolumn
-map('n', 'z.', "<cmd>lua FoldColumnExpands()<CR>", { noremap = true, silent = true }) -- Foldcolumn expends
+map("n", "z.", "<cmd>lua FoldColumnExpands()<CR>", { noremap = true, silent = true }) -- Foldcolumn expends
 
 -- Show informations
 map("n", "s", "")
@@ -78,8 +78,8 @@ map("n", "sm", "<cmd>message<CR>") -- Message
 map("n", "sr", "<cmd>reg<CR>") -- Register
 map("n", "sd", "<cmd>lua vim.diagnostic.open_float()<CR>") -- Diagnostic
 map("n", "sk", "<cmd>lua vim.lsp.buf.hover()<CR>") -- Keyword manpage
-map("n", "sl", "<cmd>lua vim.lsp.buf.signature_help()<CR>") -- Lsp signature help 
-map("n", "<leader>d", "<cmd>lua ToggleDiagnostics_qflist()<CR>" , { desc = "diagnostics list" })
+map("n", "sl", "<cmd>lua vim.lsp.buf.signature_help()<CR>") -- Lsp signature help
+map("n", "<leader>d", "<cmd>lua ToggleDiagnostics_qflist()<CR>", { desc = "diagnostics list" })
 map("n", "s<leader>", "<cmd>WhichKey <leader><CR>") -- leader key info
 
 -- <Terminal> mode key mappings
@@ -120,4 +120,4 @@ map("n", "<leader><SPACE>", "<cmd>noh<CR>", { desc = "clean search item" })
 map("n", "<leader>v", ":vert diffsplit ", { desc = "diffsplit" })
 
 -- Set the current path as the working path
-map("n", "<leader>~", "<cmd>:lcd %:p:h<CR>:echo expand(\'%:p:h\')<CR>", { desc = "change the current path" })
+map("n", "<leader>~", "<cmd>:lcd %:p:h<CR>:echo expand('%:p:h')<CR>", { desc = "change the current path" })
