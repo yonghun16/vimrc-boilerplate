efc7cc4103bec73d310f3e13dc6266ee2c9c8e00
yonghun16 <yonghun16@naver.com>
2025-07-12 11:31:16 +0900

    Update options.lua

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -21,17 +21,17 @@ vim.api.nvim_create_autocmd({ "WinLeave" }, {
 })
 
 -- tab size
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.autoindent = true
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "pug", "python", "c", "cpp", "java" },
+  pattern = { "javascript", "typescript", "html", "css", "lua", "dart", "R" },
   callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.softtabstop = 4
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
   end,
 })
 
