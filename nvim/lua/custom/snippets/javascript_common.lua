b4af9bda57bef34e9ce1bcbf57eeb0cff7e64057
yonghun16 <yonghun16@naver.com>
2025-10-03 17:42:12 +0900

    스니펫 약간 설정

--- nvim/lua/custom/snippets/javascript_common.lua
+++ nvim/lua/custom/snippets/javascript_common.lua
@@ -20,15 +20,18 @@ return {
  * Memo
  * -----------------------------------------------------------*/
 
+// [환경 설정]
 const TEST_MODE = true;
-
 let input;
 
+// [입력 처리]
 if (TEST_MODE) {{
+  // 로컬 테스트 (input.txt 파일 읽기)
   const filePath = require('path').join(__dirname, 'input.txt');
-  input = require('fs').readFileSync(filePath, 'utf-8').trim().split('\\n');
+  input = require('fs').readFileSync(filePath, 'utf-8').trim().split('\n');
 }} else {{
-  input = require('fs').readFileSync(0, 'utf-8').trim().split('\\n');
+  // 제출 환경 (표준 입력)
+  input = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
 }}
 
 ]],
--- nvim/lua/custom/snippets/python.lua
+++ nvim/lua/custom/snippets/python.lua
@@ -21,14 +21,18 @@ return {
 
 import sys
 
+# [환경 설정]
 TEST_MODE = True
 
+# [입력 처리 함수]
 def get_inputs():
+    # 로컬 테스트 (input.txt 파일 읽기)
     if TEST_MODE:
         with open('input.txt', 'r') as f:
             for line in f:
                 yield line.rstrip('\n')
     else:
+    # 제출 환경 (표준 입력)
         for line in sys.stdin:
             yield line.rstrip('\n')
 
